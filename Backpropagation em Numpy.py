0fa747eff5ded1c3e4623ef7e84a56e1bdc02e96
Vítor Borba Rodrigues <32020015+vbrodrigues@users.noreply.github.com>
2019-01-08 16:08:00 -0200

    Update Backpropagation em Numpy.py

--- Backpropagation em Numpy.py
+++ Backpropagation em Numpy.py
@@ -6,6 +6,7 @@ def sigmoid(z):
     return 1 / (1 + np.e ** (-z))
 
 def sigmoid_prime(a):
+    #Estamos considerando que "a" já passou pela sigmoide, então a derivada se dá por:
     return (a) * (1 - (a))
 
 #Hiperparâmetros
@@ -46,28 +47,50 @@ def train(X, y, w1, w2, b1, b2):
     a2 = sigmoid(z2)
     
     #BACK
-    
-        #ERROS NAS CAMADAS
     '''
     Calculamos o erro na última camada e distribuímos esse erro nos weights das camadas anteriores. 
-    Como é um passo no backprop, transpomos a matriz dos weights. Assim, temos o erro na camada anterior.'''
-    output_error = a2 - y
-    hidden_error = np.matmul(w2.T, output_error)
+    O erro do output será a2 - y. Lembrando, Cost = sum(error ** 2). Calculamos o gradiente do output multiplicando dC/derror * derror/da2 * da2/dz2. Esse 
+    gradiente será multiplicado por dz2/dW2 transposta (já que é um passo backpropagation) para achar o update vector em seguida. 
     
-        #VETORES UPDATES
+    dC/derror = d(error ** 2)/derror = 2*error = error = output_error.
+    derror/da2 = d(a2 - y)/da2 = 1
+    da2/dz2 = sigmoid_prime(a2)
+    dz2/dW2 = d(W2*a1 + b2)/dW2 = a1
     '''
-    Se cost = sum(a2 - y) **2 / n, então a dCost/da2 é 2/n*(a2 - y). Podemos ignorar o 2/n, ficando a2 - y como derivada, que é o output_error
-        dCost/da2 = a2 - y. 
-        da2/dz2 = sigmoid_prime(a2). 
-        dz2/dw2 = a1.T.
-    Enquanto as duas primeiras derivadas são element-wise, a última é uma matmul dos gradientes com a transposta da ativação anterior
-        dCost/da2 * da2/dz2 x dz2/dw2 = output_error * sigmoid_prime(a2) x a1.T
-    Assim, achamos o quanto os weights tem que ser ajustados para diminuir o cost.
-    Para os biases, o delta (o quanto devem ser ajustados) é simplesmente o gradiente já calculado, pois dz2/db2 = 1.'''
-    gradient2 = (a2 - y) * sigmoid_prime(a2) 
+    output_error = a2 - y
+    gradient2 = output_error * sigmoid_prime(a2) 
     update_vector_2 = np.matmul(gradient2, a1.T)
+    '''Enquanto as duas primeiras derivadas são element-wise, a última é uma matmul dos gradientes com a transposta da ativação anterior
+    dCost/da2 * da2/dz2 x dz2/dw2 = output_error * sigmoid_prime(a2) x a1.T
+    Assim, achamos o quanto os weights tem que ser ajustados para diminuir o cost.'''
+    
+    '''
+    Agora precisamos calcular o hidden error. Podemos pensar no hidden error de cada node em hidden como uma parcela que os pesos de cada hidden_node contribuem
+    para o output_error. Pensando assim, podemos multiplicar os pesos de cada hidden_node, ou seja W2, pelo output_error. O certo seria dividir cada W pela soma
+    de todos os W em W2 para termos realmente uma proporção (ex.: w0,0 / (w0,0 + w0,1) * output_error), mas podemos ignorar esse denominador já que iremos 
+    multiplicar tudo por um learning_rate de qualquer maneira.
+    Outra maneira é pensar nas derivadas parciais. Agora que temos o gradient2, podemos calcular o quanto a camada hidden influencia nesse gradient2, lembrando
+    que o próprio gradient2 influencia no erro final. Então precisamos calcular dC/dz1, que pela regra da cadeia se dá por:
+    dC/derror * derror/da2 * da2/dz2 * dz2/da1 * da1/dz1
+    Já temos quase todas as parcelas dessa conta vindas de gradient2, então temos gradient2 * dz2/da1 * da1/dz1, onde:
+    dz2/da1 = d(W2*a1 + b2)/da1 = W2
+    da1/dz1 = sigmoid_prime(a1)
+    No fim, organizando para as dimensões fecharem, temos que hidden_error é igual a W2.T * gradient2 * sigmoid_prime(a1). Substituíndo gradient2, ficamos
+    com:
+    hidden_error = W2.T * output_error * sigmoid_prime(a2) * sigmoid_prime(a1)
+    Aqui, consideraremos os 3 primeiros termos como hidden_error e calcularemos a multiplicação por sigmoid_prime(a1) para acharmos gradient1
+    Como é um passo no backprop, transpomos a matriz dos weights. 
+    Agora que temos gradient1, multiplicamos por dz1/dW1 para finalmente acharmos o update vector, sabendo que:
+    dz1/dW1 = d(W1*X + b1)/dW1 = X
+    Como é um passo backprop, transpomos a matriz de input X.
+    '''
+    hidden_error = np.matmul(w2.T, output_error) * sigmoid_prime(a2)
     gradient1 = hidden_error * sigmoid_prime(a1)
     update_vector_1 = np.matmul(gradient1, X.T)
+    
+    '''
+    Para os biases, o delta (o quanto devem ser ajustados) é simplesmente o gradiente já calculado, pois dz2/db2 = 1.
+    '''
     
         #AJUSTAR PARÂMETROS
     w1 -= lr * update_vector_1
